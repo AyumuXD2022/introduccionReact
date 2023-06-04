51ac4236f3f1a39a173a65fe056c8736de8a80a3
Gerardo <ayumuXD@proton.me>
2023-06-04 08:40:01 -0600

    finalizando

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import Saludar from './components/Saludar';
+import { useState,useEffect } from 'react';
 
 function App() {
+
+  const [stateCar, setStateCar] = useState(false);
+  const [contar, setContar] = useState(0);
+
+  const encenderApagar = () =>{
+    setStateCar(!stateCar);
+    setContar(contar + 1);
+  }
+
+  useEffect(()=>{
+    console.log(`Total ${contar}`)
+  },[contar])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Saludar/>
+        <h3>El coche esta: {stateCar ? "Encendido" : "Apagado"}</h3>
+        <h4>Clicks: {contar}</h4>
+        <button onClick={encenderApagar}>Encender/Apagado</button>
       </header>
     </div>
   );
--- src/components/Saludar.js
+++ src/components/Saludar.js
@@ -1,14 +1,10 @@
 import React from "react";
 
-export default function Saludar(){
-
-    const saludar = () => {
-        alert("Hola mundo")
-    }
+export default function Saludar({saludar,userInfo : {nombre = "Anonimo", edad,color}}){
 
     return (
         <>
-            <button onClick={saludar}>Saludar</button>
+            <button onClick={()=>saludar(nombre,edad)}>Saludar</button>
         </>
     )
 }
